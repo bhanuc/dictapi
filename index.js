632987dda773c40f7bdae201b3ef118806b51d10
bhanuc <bhanuc@iitk.ac.in>
2014-09-28 14:40:28 +0530

    step two

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 var koa = require('koa');
 var app = koa();
+var router = require('koa-router');
+var mount = require('koa-mount');
 
 
-app.use(function *(next){
+var handler = function *(next){
     this.type = 'json';
     this.status = 200;
     this.body = {'Welcome': 'This is a level 2 Hello World Application!!'};
-});
+};
 
+var APIv1 = new router();
+APIv1.get('/all', handler);
+
+app.use(mount('/v1', APIv1.middleware()));
 if (!module.parent) app.listen(3000);
 console.log('Hello World is Running on http://localhost:3000/');
